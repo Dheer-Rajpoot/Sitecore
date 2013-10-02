09aa675bd49e882d8d49ac31053ed0c5725bd169
Dheer-Rajpoot <rajpoot.dheer@gmail.com>
2013-10-02 01:33:22 -0700

    Sitecore: Kick idle or inactive users automatically

---
+++ Sitecore.KickUserSessionAgent/KickUserSession.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Sitecore.Web.Authentication;
+using System.Collections.Generic;
+
+namespace Sitecore.KickUserSessionAgent
+{
+    public class KickUserSession
+    {
+        private TimeSpan maximumIdleTime;
+
+        public KickUserSession(string maximumIdleTime)
+        {
+            this.maximumIdleTime = TimeSpan.Parse(maximumIdleTime);
+        }
+
+        public void Run()
+        {
+            List<DomainAccessGuard.Session> userSessionList = DomainAccessGuard.Sessions;
+
+            if (userSessionList != null && userSessionList.Count > 0)
+            {
+                foreach (DomainAccessGuard.Session userSession in userSessionList.ToArray())
+                {
+                    TimeSpan span = (TimeSpan)(DateTime.Now - userSession.LastRequest);
+                    if (span > this.maximumIdleTime)
+                    {
+                        DomainAccessGuard.Kick(userSession.SessionID);
+                        Sitecore.Diagnostics.Log.Audit("Kicked out user is : " + userSession.UserName, this);
+                    }
+                }
+            }
+        }
+    }
+}
